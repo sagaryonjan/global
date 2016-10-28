a252a59c36454308524d483901338e3f18408e96
sagaryonjan <sagaryonjan015@gmail.com>
2016-10-28 08:40:19 +0545

    css

--- template-parts/content.php
+++ template-parts/content.php
@@ -10,32 +10,33 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
 	<div class="ts-image">
 		<figure class="ts-single-image">
 			<?php if(has_post_thumbnail()):
 				the_post_thumbnail();
 			endif;
 			?>
-		</figure>
-		<header class="entry-header">
+		</figure> 
+		<?php
+		if ( 'post' === get_post_type() ) : ?>
+			<div class="entry-meta">
+				<?php global_posted_on(); ?>
+			</div><!-- .entry-meta -->
 			<?php
-			if ( is_single() ) :
-				the_title( '<h1 class="entry-title">', '</h1>' );
-			else :
-				the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
-			endif;
-
-			if ( 'post' === get_post_type() ) : ?>
-				<div class="entry-meta">
-					<?php global_posted_on(); ?>
-				</div><!-- .entry-meta -->
-				<?php
-			endif; ?>
-		</header><!-- .entry-header -->
+		endif; ?>
 	</div>
 
 
-
+	<header class="entry-header">
+		<?php
+		if ( is_single() ) :
+			the_title( '<h1 class="entry-title">', '</h1>' );
+		else :
+			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
+		endif;
+		?>
+	</header><!-- .entry-header -->
 
 	<div class="entry-content">
 		<?php
